785a3b90b12de4a5690e5fcb37e77e9dc402c8e7
Robin <robinniwood@gmail.com>
2021-05-10 11:06:27 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,8 @@ git clone https://github.com/Niwood/DQL-Trader.git
 # Pull latest from github                                                       
 cd DQL-Trader/ && git pull
 
+# Switch branch to EC2 supported
+git checkout aws-ec2-supported
 
 # Update pip                                                                    
 #sudo pip3 install --upgrade pip
