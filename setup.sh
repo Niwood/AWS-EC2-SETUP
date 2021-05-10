25bd90acacfba23b1ba94bc9ea0be3033db19b2b
Robin <robinniwood@gmail.com>
2021-05-10 10:55:50 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,18 @@
-# DQL SETUP FILE FOR AWS EC2 INSTANCE
+#!/bin/sh                                                                       
+echo "--- This a setup script for DQL Trader for AWS EC2 instance ---"
+
+# Update                                                                        
+sudo yum update -y
+
+# Install git                                                                   
+sudo yum install git -y
+
+# Download repo                                                                 
+git clone https://github.com/Niwood/DQL-Trader.git
+
+# Pull latest from github                                                       
+cd DQL-Trader/ && git pull
+
+
+# Update pip                                                                    
+#sudo pip3 install --upgrade pip
